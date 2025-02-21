6e8df0c1717431def9f8d5752f89bde76dd2dd36
mugishasam123 <mugishasamuel42@gmail.com>
2025-02-21 17:18:11 +0200

    initial commit

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Counter from './components/Counter'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
@@ -16,6 +17,7 @@ function App() {
       <MainContent />
       <Footer />
       <UserProfile name='Alice' age='25' bio='Loves hiking and photography' />
+      <Counter />
     </div>
   )
 }
