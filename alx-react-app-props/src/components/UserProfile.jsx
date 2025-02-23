84fe2ab2a4ad60c31290d76e1b2e713aa860683a
mugishasam123 <mugishasamuel42@gmail.com>
2025-02-23 14:04:08 +0200

    initial commit

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,14 +1,13 @@
-import { useContext } from "react"
 function UserProfile({userData}){
     return (
         
-        <useContext.Provider>
+        <UserContext.Provider>
             <div>
                 <h2>{userData.name}</h2>
                 <p>Age: {userData.age}</p>
                 <p>Bio: {userData.bio}</p>
             </div>
-        </useContext.Provider>
+        </UserContext.Provider>
     )
 }
 export default UserProfile
