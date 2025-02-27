8ca0b3e3a4c9a24185848e1e6995ce21674cbc87
mugishasam123 <mugishasamuel42@gmail.com>
2025-02-27 13:56:07 +0200

    initial commit

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -4,6 +4,7 @@ import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetails from './components/RecipeDetails';
 import SearchBar from './components/SearchBar';
 import FavoritesList from './components/FavoritesList';
+import RecommendationsList from './components/RecommendationsList'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <SearchBar />
         <AddRecipeForm />
         <FavoritesList />
+        <RecommendationsList />
         <Routes>
           <Route path="/" element={<RecipeList />} />
           <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
