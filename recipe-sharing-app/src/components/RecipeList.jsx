e82b145e105cac8b6bfad8ee216f5ad7395b2f28
mugishasam123 <mugishasamuel42@gmail.com>
2025-02-27 13:12:32 +0200

    initial commit

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -2,12 +2,12 @@ import { Link } from 'react-router-dom';
 import useRecipeStore from '../components/recipeStore';
 
 const RecipeList = () => {
-  const recipes = useRecipeStore(state => state.recipes);
+  const filteredRecipes = useRecipeStore(state => state.filteredRecipes.length ? state.filteredRecipes : state.recipes);
 
   return (
     <div>
       <h2>Recipe List</h2>
-      {recipes.map(recipe => (
+      {filteredRecipes.map(recipe => (
         <div key={recipe.id}>
           <h3>{recipe.title}</h3>
           <p>{recipe.description}</p>
