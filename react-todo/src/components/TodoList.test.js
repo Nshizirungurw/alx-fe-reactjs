ed87a3a1468a23ec8b428b4aa36d99a37c98febd
mugishasam123 <mugishasamuel42@gmail.com>
2025-03-08 16:40:39 +0200

    initial commit

---
+++ react-todo/src/components/TodoList.test.js
@@ -0,0 +1,34 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import TodoList from '../TodoList';
+
+test('renders TodoList with initial todos', () => {
+  render(<TodoList />);
+  expect(screen.getByText('Learn React')).toBeInTheDocument();
+  expect(screen.getByText('Write Tests')).toBeInTheDocument();
+});
+
+test('can add a new todo', () => {
+  render(<TodoList />);
+  const input = screen.getByRole('textbox');
+  const button = screen.getByText('Add Todo');
+
+  fireEvent.change(input, { target: { value: 'New Todo' } });
+  fireEvent.click(button);
+
+  expect(screen.getByText('New Todo')).toBeInTheDocument();
+});
+
+test('can toggle a todo', () => {
+  render(<TodoList />);
+  const todo = screen.getByText('Learn React');
+  fireEvent.click(todo);
+  expect(todo).toHaveStyle('text-decoration: line-through');
+});
+
+test('can delete a todo', () => {
+  render(<TodoList />);
+  const todo = screen.getByText('Learn React');
+  const deleteButton = todo.nextSibling;
+  fireEvent.click(deleteButton);
+  expect(screen.queryByText('Learn React')).toBeNull();
+});
