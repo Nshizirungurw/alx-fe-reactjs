a04f4d93e2f81dd6259c68c159f5261447eb1a34
mugishasam123 <mugishasamuel42@gmail.com>
2025-03-10 20:43:22 +0200

    initial commit

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -2,7 +2,7 @@ import './index.css'
 function App() {
   return (
       <div>
-        <h1 className="text-3xl font-bold underline">Hello,This is taiwind css </h1>
+        <h1 className="text-3xl pl-4 font-bold underline">Hello,This is taiwind css </h1>
       </div>
  
   )
