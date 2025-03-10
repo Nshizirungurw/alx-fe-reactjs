c60bb66f3c85886179f06f2f226a5acaaefa777f
mugishasam123 <mugishasamuel42@gmail.com>
2025-03-10 21:57:19 +0200

    initial commit

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -3,7 +3,7 @@ import '../index.css'
 function UserProfile() {
     return (
       <div className="user-profile sm:p-4 bg-gray-100 mx-auto my-20 rounded-lg shadow-lg md:p-8 md:max-w-sm max-w-xs">
-        <img src="https://via.placeholder.com/150" alt="User" className='h-24 w-24 rounded-full md:w-36 md:h-36 mx-auto' />
+        <img src="https://via.placeholder.com/150" alt="User" className='sm:h-24 sm:w-24 rounded-full md:w-36 md:h-36 mx-auto' />
         <h1 className=' text-lg md:text-xl text-blue-800 my-4 font-bold'>John Doe</h1>
         <p className='text-sm text-gray-600 md:text-base'>Developer at Example Co. Loves to write code and explore new technologies.</p>
       </div>
