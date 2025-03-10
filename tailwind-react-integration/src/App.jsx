49604cadb909c5a259ad64d66f700ccff622470e
mugishasam123 <mugishasamuel42@gmail.com>
2025-03-10 20:55:30 +0200

    initial commit

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,8 +1,9 @@
+import UserProfile from './components/UserProfile'
 import './index.css'
 function App() {
   return (
       <div>
-        <h1 className="text-3xl pl-4 font-bold underline">Hello,This is taiwind css </h1>
+        <UserProfile />
       </div>
  
   )
